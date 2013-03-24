5f44450a7c9e21743db801a6857a038b12e0e565
Eildosa <thongvan.alexis@gmail.com>
2013-03-24 11:46:03 +0100

    Main functions definition

---
+++ downloadCleaner.py
@@ -0,0 +1,71 @@
+'''
+Created on 24 mars 2013
+
+@author: Eildosa
+
+delete all unwanted files (rar, par, .1, .idx, etc)
+rename file with a tag ex : [1080p-Web]
+move file from unrecognised folder to a common location
+delete empty folders
+
+Do not rename files when resolution is not available in file-name
+
+Used for sabNzbs completed folder.
+
+recognized resolution :
+
+480p (if contains hdtv and no other resolution then it's also 480p)
+560p
+720p
+1080p
+
+recognized quality
+
+hdtv
+web
+dvdrip
+bluray
+bdrip
+
+'''
+
+
+
+    def get_all_files_from(path):
+        return None
+    
+    def get_all_folders_from(path):
+        pass
+    
+    def get_all_files_from_folder_and_sub_folders(path):
+        pass
+    
+    def move_to_trash(trash_path, file_path):
+        pass
+    
+    def delete_folder(folder_path):
+        pass
+    
+    def delete_file(file_path):
+        pass
+    
+    def determine_if_bad_file(file_name):
+        pass
+    
+    def determine_if_is_in_bad_folder(file_path):
+        pass
+    
+    def determine_resolution(file_name):
+        pass
+    
+    def determine_quality(file_name):
+        pass
+    
+    def determine_if_file_need_renaming(file_name):
+        pass
+    
+    def is_folder_empty(folder_path):
+        pass
+    
+    #code goes here
+    
